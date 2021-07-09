5342c53edfe5d2d60ec1ee211f4e28daa382de3b
Francesca Negri <fnegri910@gmail.com>
2021-07-09 13:30:21 +0200

    First settings & params implementation

--- pde/util/parameters.py
+++ pde/util/parameters.py
@@ -4,9 +4,9 @@ L=10
 J=101
 cf=0.5
 
-u = []
-un,un2,norme = [],[],[]
-err=[]
+# u = []
+# un,un2,norme = [],[],[]
+# err=[]
 
 deltax= L/(J-1)
 deltat= (deltax * cf)/a
---
+++ pde/util/settings.py
@@ -0,0 +1,8 @@
+import os
+
+UTIL_PATH = os.path.dirname(__file__)
+CODE_PATH = os.path.abspath(os.path.join(UTIL_PATH, '..'))
+TOP_LEVEL_DIR = os.path.abspath(os.path.join(CODE_PATH, '..'))
+DATA_PATH = os.path.join(TOP_LEVEL_DIR, 'data')
+
+print('ok')
