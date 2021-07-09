06719f62e02e576966d5ccf6fcabd446cd66fb77
Francesca Negri <fnegri910@gmail.com>
2021-07-09 13:30:49 +0200

    Added first functions for base plot and ftcs

--- pde/util/Functions.py
+++ pde/util/Functions.py
@@ -1,35 +1,59 @@
 import numpy as np 
+from numpy import linalg as LA
 import util.parameters as pr
+import matplotlib.pyplot as plt
+import util.settings as stg
+import os
 
 class Functions:
 
     def __init__(self):
         
+        def _indexes():
+            a= np.asarray([i for i in range(0,self.LEN)])
+            b= np.roll(a,1)
+            c= np.roll(a,-1)
+            return a,b,c
+        
         self.deltax = pr.L/(pr.J-1)
         self.deltat = (self.deltax * pr.cf) / pr.a
-    
+
         self.x_val = np.arange(0,pr.L,self.deltax)
         
         self.gauss = np.exp(-np.power(self.x_val-pr.x0,2))
     
         self.LEN = len(self.x_val)
+        self.j,self.jmin,self.jmax = _indexes()
         
-    def _indexes(self):
-        a= [i for i in range(0,self.LEN)]
-        b= np.roll(a,1)
-        c= np.roll(a,-1)
-        return a,b,c
-    
-    def testresult(self):
-        j,jmin,jmax = self.indexes()
-        g= self.gauss
-        for a,b,c in zip(j,jmin,jmax):
-            print(g[a],g[b],g[c])
+        self.coefficient = (pr.a*self.deltat)/(2*self.deltax)
             
-    def ftcs(self):
-        j,jmin,jmax = self.indexes()
-        g = self.gauss
-        coefficient = (self.a*self.deltat)/(2*self.deltax)
-        print(coefficient)
-        curve = [ g[a] - (coefficient)*(g[b]-g[c]) for a,b,c in zip(j,jmin,jmax)]
+    def get_gaussian(self):
+        return self.gauss
+    
+    def ftcs(self,v=None):
+        if v is None: v= self.get_gaussian()
+        curve = [ v[a] - (self.coefficient)*(v[b]-v[c]) for a,b,c in zip(self.j,self.jmax,self.jmin)]
+        
         return curve
+
+    def get_norm(v,self):
+        return LA.norm(v)/np.sqrt(pr.J)
+    
+    def cartesian_plot(self,*args):
+        x,y = args
+        plt.plot(x,y)
+        
+    def pre_plot(self,n):
+        fig=plt.figure(n)
+
+
+    def plot(self,data):
+        plt.plot(*data)
+
+
+    def post_plot(self,*args):
+        xlb,ylb,title,figname= args
+        plt.xlabel(xlb)
+        plt.ylabel(ylb)
+        plt.title(title)
+        plt.savefig(os.path.join(stg.DATA_PATH, figname), dpi=100)
