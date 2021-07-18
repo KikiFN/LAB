32ac29459016b8a0b4233ec481bfcf21ca652aa0
Francesca Negri <fnegri910@gmail.com>
2021-07-18 16:34:13 +0200

    Deleted tests

--- pde/util/settings.py
+++ pde/util/settings.py
@@ -4,5 +4,3 @@ UTIL_PATH = os.path.dirname(__file__)
 CODE_PATH = os.path.abspath(os.path.join(UTIL_PATH, '..'))
 TOP_LEVEL_DIR = os.path.abspath(os.path.join(CODE_PATH, '..'))
 DATA_PATH = os.path.join(TOP_LEVEL_DIR, 'data')
-
-print('ok')
